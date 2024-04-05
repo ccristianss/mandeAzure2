ea1a803ecd772c44a7e01833bd48585f07b01090
ccristianss <cristian.silva9402@gmail.com>
2024-04-05 04:13:53 -0500

    Allowed Hosts

--- mande/settings.py
+++ mande/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5k+u__9_ea*-%q!e&%fjf9k2%8brk7*=il(fq1i28!d37*4_7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'manders.azurewebsites.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'manders.azurewebsites.net', '*']
 
 
 # Application definition
