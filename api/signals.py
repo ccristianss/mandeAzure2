853d05af8623edbe76e905a09170d07a2802d4a4
ccristianss <cristian.silva9402@gmail.com>
2024-05-01 18:59:35 -0500

    Se actualiza Backend para notificaciones

--- api/signals.py
+++ api/signals.py
@@ -2,24 +2,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Requestmanager, Request, Mander, User
 from firebase_config import messaging, db
+import datetime
 
-
-@receiver(post_save, sender=Requestmanager)
-def update_request_status(sender, instance, created, **kwargs):
-    if created:
-        # Obtener el objeto Request asociado al Requestmanager
-        related_request = instance.request_id_request
-
-        # Actualizar el status_request a 'Proceso'
-        related_request.status_request = 'Proceso'
-        related_request.save()
-        
-    if instance.status_requestmanager == 'terminado':
-        # Obtener el objeto Request relacionado
-        request_instance = instance.request_id_request
-        # Actualizar el campo status_request a 'Finalizado'
-        request_instance.status_request = 'Finalizado'
-        request_instance.save()
 
 @receiver(post_save, sender=Mander)
 def update_user_mander(sender, instance, created, **kwargs):
@@ -41,46 +25,71 @@ def delete_user_mander(sender, instance, **kwargs):
 @receiver(post_save, sender=Request)
 def send_notification_on_request_creation(sender, instance, created, **kwargs):
     if created:
-        # Crea el mensaje de notificación
-        message = messaging.Message(
-            notification=messaging.Notification(
-                title='Nuevo pedido creado',
-                body=f'Se ha creado un nuevo pedido: {instance.detail_request}'
-            ),
-            android=messaging.AndroidConfig(
-                priority='high',
-                notification=messaging.AndroidNotification(
-                    priority='high',
-                ),
-            ),
-            topic='new_request_popayan',   # Puedes enviar la notificación a un tema específico
-        )
-
+        title='NUEVO MANDADO'
+        body=f'Detalle: {instance.detail_request}'
+        idrequest=f'{instance.id_request}'
+        topic='new_request_popayan'
+        message = message_to_manders(title, body, topic, idrequest)
         response = messaging.send(message)
         print('Notificación enviada:', response)
 
-@receiver(post_save, sender=Request)
-def send_notification_on_request_update(sender, instance, **kwargs):
-    user_id = instance.user_id_user_id
-    print(user_id)
+def message_to_manders(mTitle, mBody, mTopic, mIdRequest):
+    message = messaging.Message(
+        data={
+            'title': mTitle,
+            'body': mBody,
+            'idrequest': mIdRequest,
+            'to': 'mander',
+        },
+        topic=mTopic,
+    )
+    return message
+
+@receiver(post_save, sender=Requestmanager)
+def update_request_status(sender, instance, created, **kwargs):
+    if created:
+        related_request = instance.request_id_request
+        related_request.status_request = 'Proceso'
+        related_request.save()
+        idrequest = f'{related_request.id_request}'
+        detailrequest = f'{related_request.detail_request}'
+        iduser = f'{related_request.user_id_user_id}'
+        statusrequest = f'{related_request.status_request}'
+        send_notification_user(idrequest, detailrequest, iduser, statusrequest)
+        
+    if instance.status_requestmanager == 'terminado':
+        request_instance = instance.request_id_request
+        request_instance.status_request = 'Finalizado'
+        request_instance.save()
+        idrequest = f'{request_instance.id_request}'
+        detailrequest = f'{request_instance.detail_request}'
+        iduser = f'{request_instance.user_id_user_id}'
+        statusrequest = f'{request_instance.status_request}'
+        send_notification_user(idrequest, detailrequest, iduser, statusrequest)
+
+def send_notification_user(idrequest, detailrequest, iduser, statusrequest):
+    user_id = iduser
+    #print(user_id)
     ref = db.reference(f'Manders/Tokens/{user_id}/token')
     token = ref.get()
-
     if token:
-        message = messaging.Message(
-            notification=messaging.Notification(
-                title='Actualización de solicitud',
-                body=f'Se ha actualizado la solicitud {instance.detail_request}',
-            ),
-            android=messaging.AndroidConfig(
-                priority='high',
-                notification=messaging.AndroidNotification(
-                    priority='high',
-                ),
-            ),
-            token=token,
-        )
+        title = f'Actualización --> {detailrequest}.'
+        body = f'Estado: {statusrequest}.'
+        message = message_to_user(title, body, token, idrequest)
+        #print(message)
         response = messaging.send(message)
         print('Notification sent:', response)
     else:
         print(f'No se encontró el token para el usuario con user_id: {user_id}')
+
+def message_to_user(mTitle, mBody, mtoken, mIdRequest):
+    message = messaging.Message(
+        data={
+            'title': mTitle,
+            'body': mBody,
+            'idrequest': mIdRequest,
+            'to': 'user',
+        },
+        token=mtoken,
+    )
+    return message
