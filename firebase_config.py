fe4522fcddec6e982d28c1981bb1500b0d6d93a8
ccristianss <cristian.silva9402@gmail.com>
2024-05-07 04:01:01 -0500

    update detailManders

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,13 @@
+import json
+import os
+from base64 import b64decode
 import firebase_admin
 from firebase_admin import credentials, messaging, db
 
 
-cred = credentials.Certificate('serviceAccountKey.json')
+firebase_key_path = os.getenv('FIREBASE_KEY_JSON')
+firebase_dict = json.loads(b64decode(firebase_key_path))
+cred = credentials.Certificate(firebase_dict)
 firebase_config = firebase_admin.initialize_app(cred, {
     'databaseURL':  'https://mandersdev-default-rtdb.firebaseio.com'
 })
